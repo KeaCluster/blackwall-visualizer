c3ce7c83e54ed887490e75e940d0fa0357ff595b
KeaCluster <zzabdiel29@gmail.com>
2024-09-05 19:32:47 -0600

    Cleaned up hook, optimized for playback when paused/resumed

--- src/hooks/useAudioPlayer.ts
+++ src/hooks/useAudioPlayer.ts
@@ -1,127 +1,80 @@
 import { useState, useEffect, useRef } from "react";
 
-export function useAudioPayer(audioFile: File | null) {
-  const [analyser, setAnalyser] = useState<AnalyserNode | null>(null);
-  const [dataArray, setDataArray] = useState<Uint8Array | null>(null);
+export function useAudioPlayer(audioFile: File | null) {
   const [isPlaying, setIsPlaying] = useState<boolean>(false);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [volume, setVolume] = useState<number>(0.3); // volume
   const [currentTime, setCurrentTime] = useState<number>(0);
   const [duration, setDuration] = useState<number>(0);
-  const [error, setError] = useState<string>("");
 
   const audioContextRef = useRef<AudioContext | null>(null);
   const sourceRef = useRef<AudioBufferSourceNode | null>(null);
+  const analyserRef = useRef<AnalyserNode | null>(null);
   const gainNodeRef = useRef<GainNode | null>(null); // one input one output for volume
+  const dataArrayRef = useRef<Uint8Array | null>(null);
+
   const startTimeRef = useRef<number>(0);
-  const pausedTimeRef = useRef<number>(0);
+  const offsetRef = useRef<number>(0);
 
   useEffect(() => {
-    // return type for specific type
-    let intervalId: ReturnType<typeof setTimeout>;
-
     if (audioFile) {
-      // context and compatibility
-      if (!window.AudioContext) {
-        setError("Your browser does not support the Web Audio API.");
-      }
       // prepare data
-      setIsLoading(true);
       const audioContext = new AudioContext();
       audioContextRef.current = audioContext;
 
       // prepare file reader
       const reader = new FileReader();
-      reader.onload = function (event) {
-        // assertion
+      reader.onload = async (event) => {
         const arrayBuffer = event.target?.result as ArrayBuffer;
-        audioContext.decodeAudioData(
-          arrayBuffer,
-          (audioBuffer) => {
-            const source = audioContext.createBufferSource();
-            source.buffer = audioBuffer;
+        try {
+          // assertion
+          const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+          setDuration(audioBuffer.duration);
 
-            // node and arr  will help for visualizing data
-            const analyserNode = audioContext.createAnalyser();
-            analyserNode.fftSize = 2048; // default frequency
-            const bufferLength = analyserNode.frequencyBinCount;
-            const dataArray = new Uint8Array(bufferLength);
+          const analyserNode = audioContext.createAnalyser();
+          analyserNode.fftSize = 2048;
+          const dataArray = new Uint8Array(analyserNode.frequencyBinCount);
 
-            const gainNode = audioContext.createGain();
-            gainNode.gain.value = volume;
-            gainNodeRef.current = gainNode;
+          const gainNode = audioContext.createGain();
+          gainNode.gain.value = volume;
 
-            source.connect(analyserNode);
-            analyserNode.connect(gainNode);
-            gainNode.connect(audioContext.destination);
+          analyserRef.current = analyserNode;
+          dataArrayRef.current = dataArray;
+          gainNodeRef.current = gainNode;
 
-            source.start(0);
-            startTimeRef.current = audioContext.currentTime;
-            setIsPlaying(true);
-            setDuration(audioBuffer.duration);
-
-            setAnalyser(analyserNode);
-            setDataArray(dataArray);
-            sourceRef.current = source;
-            setIsLoading(false); // Full load
-
-            // Timer per sec
-            intervalId = setInterval(() => {
-              if (audioContextRef.current && isPlaying) {
-                const elapsed =
-                  audioContextRef.current.currentTime -
-                  startTimeRef.current +
-                  pausedTimeRef.current;
-                setCurrentTime(elapsed);
-                if (elapsed >= audioBuffer.duration) {
-                  setIsPlaying(false);
-                  clearInterval(intervalId);
-                }
-              }
-            }, 500);
-          },
-          (error) => {
-            console.error("Error decoding data", error);
-            setError("Failed to decode audio data");
-            setIsLoading(false);
-          },
-        );
-      };
-      reader.readAsArrayBuffer(audioFile);
-
-      // new file
-      return () => {
-        if (sourceRef.current) {
-          sourceRef.current.stop();
+          playAudio(audioBuffer, offsetRef.current);
+          return () => {
+            stopAudio();
+            audioContext.close();
+          };
+        } catch (error) {
+          console.error("Error decoding audio data", error);
         }
-        if (audioContextRef.current) {
-          audioContextRef.current.close();
-        }
-        setIsPlaying(false);
-        setAnalyser(null);
-        setDataArray(null);
-        setVolume(0.3);
-        setCurrentTime(0);
-        setDuration(0);
-        setError("");
-        clearInterval(intervalId);
+        reader.readAsArrayBuffer(audioFile);
       };
     }
   }, [audioFile]);
 
-  const togglePlay = () => {
-    if (audioContextRef.current) {
-      if (isPlaying) {
-        audioContextRef.current.suspend();
-        pausedTimeRef.current +=
-          audioContextRef.current.currentTime - startTimeRef.current; // pause for gainNode context
-        setIsPlaying(false);
-      } else {
-        audioContextRef.current.resume();
-        startTimeRef.current = audioContextRef.current.currentTime;
-        setIsPlaying(true);
-      }
-    }
+  // new Play func
+  const playAudio = (audioBuffer: AudioBuffer, offset: number) => {
+    const audioContext = audioContextRef.current;
+    if (!audioContext) return;
+
+    const source = audioContext.createBufferSource();
+    source.buffer = audioBuffer;
+    source.connect(analyserRef.current!);
+    analyserRef.current!.connect(gainNodeRef.current!);
+    gainNodeRef.current!.connect(audioContext.destination);
+
+    source.start(0, offset);
+    sourceRef.current = source;
+    startTimeRef.current = audioContext.currentTime - offset;
+    setIsPlaying(true);
+    updateCurrentTime();
+  };
+
+  const stopAudio = () => {
+    sourceRef.current?.stop();
+    setIsPlaying(false);
   };
 
   const changeVolume = (value: number) => {
@@ -131,16 +84,42 @@ export function useAudioPayer(audioFile: File | null) {
     }
   };
 
+  function updateCurrentTime() {
+    if (!isPlaying) return;
+    const audioContext = audioContextRef.current;
+    if (audioContext) {
+      const elapsed = audioContext.currentTime - startTimeRef.current;
+      setCurrentTime(offsetRef.current + elapsed);
+      if (offsetRef.current + elapsed >= duration) {
+        setIsPlaying(false);
+      } else {
+        requestAnimationFrame(updateCurrentTime);
+      }
+    }
+  }
+
+  const togglePlay = () => {
+    if (!audioContextRef.current || !sourceRef.current) return;
+    if (isPlaying) {
+      stopAudio();
+      offsetRef.current +=
+        audioContextRef.current.currentTime - startTimeRef.current;
+    } else {
+      playAudio(sourceRef.current.buffer!, offsetRef.current);
+    }
+  };
+
+  const analyser = analyserRef.current;
+  const dataArray = dataArrayRef.current;
+
   return {
     analyser,
     dataArray,
     isPlaying,
     togglePlay,
-    isLoading,
     volume,
     changeVolume,
     currentTime,
     duration,
-    error,
   };
 }
